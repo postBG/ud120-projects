969463eee2b6e5aa0e75e967442ceec83fddf7de
unknown <profile2697@gmail.com>
2015-09-14 01:47:36 +0900

    implemeted outliear cleaner with vectorization

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,12 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    error = predictions - net_worths
+    sq_error = error * error
 
-    
+    not_outliears = sq_error.flatten().argsort()[:-10]
+    for idx in not_outliears:
+        cleaned_data.append((ages[idx], net_worths[idx], error[idx]))
+
     return cleaned_data
 
