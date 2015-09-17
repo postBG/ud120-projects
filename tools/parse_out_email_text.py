db09a00ac5f4d327a44056308d14d7b3f3222edd
unknown <profile2697@gmail.com>
2015-09-17 23:31:15 +0900

    Augment parseOutText() with SnowballStemmer

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,16 +28,23 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        word_list = text_string.split()
 
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer("english")
+        stemmed_words = []
+        for word in word_list:
+            stemmed_words.append(stemmer.stem(word))
 
+        for word in stemmed_words:
+            words = words + ' ' + word.encode('ascii', 'ignore')
 
-
+        words = words.strip()
     return words
 
     
